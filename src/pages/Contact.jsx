55928ddda2e45d9aa770eb0ae0134f7632c1c74b
Logan Brignac <logan8578@gmail.com>
2025-10-06 19:07:14 -0500

    Completed Skills Page, Removed Redundant Code from contact page and added temp image to contact page in place of headshot

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,10 @@ export default function Contact() {
     <div className='main-Container'>
       <h1 className='header'>Contact Me!</h1>
       <a className='email' href='ma&#105;&#108;&#116;o&#58;logan&#56;&#53;78&#64;&#103;mai&#37;6C%2&#69;&#99;&#37;&#54;&#70;&#109;'>l&#111;g&#97;n8578&#64;gma&#105;&#108;&#46;com</a>
+      <br />
       <a className='linkedIn' href="https://www.linkedin.com/in/logan-brignac-671755347?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BO22fzR8LTV%2BinPHty0HKzA%3D%3D">LinkedIn</a>
-      <img className='headShot' src="" alt="headshot" />
+      <br />
+      <img className='headShot' src="src/images/temp3.png" alt="headshot" />
       <Link to="/" className="back-button">
                 <span> &larr;Home</span>
               </Link>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,8 +4,29 @@ import "./Skills.css";
 
 export default function Skills() {
   return (
-    <div className='main-Container'>
-      <Link to="/" className="back-button">
+    <div className='main'>
+     <div className='secondary-container'>
+      <div className='languages-container'>Languages
+        <div className='languages'>Python</div>
+        <div className='languages'>Java</div>
+        <div className='languages'>HTML</div>
+        <div className='languages'>CSS</div>
+      </div>
+      <br />
+      <div className='ides-container'>IDEs
+        <div className='ides'>Visual Studio Code</div>
+        <div className='ides'>IntelliJ</div>
+      </div>
+      <br />
+      <div className='applications-container'>Applications
+        <div className='applications'>GitHub</div>
+      </div>
+      <br />
+      <div className='frameworks-container'>Frameworks
+        <div className='frameworks'>React</div>
+      </div>
+     </div>
+     <Link to="/" className="back-button">
                 <span> &larr;Home</span>
               </Link>
     </div>
