cafc9ec04e5f57523825ab844b1176d57577ff56
Logan Brignac <logan8578@gmail.com>
2025-10-28 19:08:00 -0500

    Updated Resume, added temporary headshot, removed unecessary comments

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ import "./Resume.css";
 export default function Resume() {
   return (
     <div className='main-Container'>
-      <embed src="../images/LBrignacResume2025.pdf" type="application/pdf" width="100%" height="800px"/>
+      <embed className='resumepdf' src="../images/LBrignacR2025.pdf" type="application/pdf" width="100%" height="800px"/>
       <Link to="/" className="back-Button">
                 <span> &larr;Home</span>
               </Link>
